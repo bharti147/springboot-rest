c5a9dae2240d765637cafe33fbabb17a4b2d2f05
Bharti <sahanibharti71@gmail.com>
2026-03-30 17:29:41 +0530

    generated jwt token and validating it on subsequent requests from client

--- src/main/java/com/sb/springboot_rest/config/SecurityConfig.java
+++ src/main/java/com/sb/springboot_rest/config/SecurityConfig.java
@@ -3,15 +3,19 @@ package com.sb.springboot_rest.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +24,8 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
-
-
+    @Autowired
+    private JwtFilter jwtFilter;
 
 
     @Bean
@@ -32,25 +36,22 @@ public class SecurityConfig {
     }
 
 
-
-
-
-
-
-
-
-
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //lambda way
         http.csrf(customizer -> customizer.disable())
         .authorizeHttpRequests(request -> request
-                .requestMatchers("/register").permitAll()
+                .requestMatchers("/register", "/login").permitAll()
                 .anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+
     }
 
 
--- src/main/java/com/sb/springboot_rest/controller/UserController.java
+++ src/main/java/com/sb/springboot_rest/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.sb.springboot_rest.controller;
 import com.sb.springboot_rest.model.User;
+import com.sb.springboot_rest.service.JwtService;
 import com.sb.springboot_rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +16,26 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/register")
-    public User register(@RequestBody User user){
+    public User register(@RequestBody User user) {
         return userService.saveUser(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(user.getUsername());
+        } else {
+            return "Login failed";
+        }
     }
 }
